dc91dab0753753e8120561d07bb2625dcdb7b1fb
momo <yangmo8863@126.com>
2018-08-13 10:35:56 +0800

    second commit

---
+++ src/js/index.js
@@ -0,0 +1,53 @@
+
+$('.home').click(function () {
+  $('.home').addClass('active');
+  $('.convert').removeClass('active');
+  $('.about').removeClass('active');
+  $('#line').addClass('one');
+  $('#line').removeClass('two');
+  $('#line').removeClass('three');
+});
+$('.convert').click(function () {
+  $('.convert').addClass('active');
+  $('.home').removeClass('active');
+  $('.about').removeClass('active');
+  $('#line').addClass('two');
+  $('#line').removeClass('one');
+  $('#line').removeClass('three');
+});
+$('.about').click(function () {
+  $('.about').addClass('active');
+  $('.convert').removeClass('active');
+  $('.home').removeClass('active');
+  $('#line').addClass('three');
+  $('#line').removeClass('two');
+  $('#line').removeClass('one');
+});
+$('.home').click(function () {
+  $('#first').addClass('active');
+  $('#second').removeClass('active');
+  $('#third').removeClass('active');
+  $('#fourth').removeClass('active');
+});
+$('.convert').click(function () {
+  $('#first').removeClass('active');
+  $('#second').addClass('active');
+  $('#third').removeClass('active');
+  $('#fourth').removeClass('active');
+});
+$('.about').click(function () {
+  $('#first').removeClass('active');
+  $('#second').removeClass('active');
+  $('#third').addClass('active');
+  $('#fourth').removeClass('active');
+});
+var zerorpc = require("zerorpc");
+var client = new zerorpc.Client({ timeout: 3600*24, heartbeatInterval: 3600*1000*24});
+client.connect("tcp://127.0.0.1:42142");
+client.invoke("train", (error, res) =>{
+  if(error) {
+    console.log(error);
+  } else {
+    console.log('success');
+  }
+})
